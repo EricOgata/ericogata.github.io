6c0ea6d1d751171ec06d8d9e90e94c0f706bb999
Eric Ogata <dev.ericogata@gmail.com>
2024-09-09 22:12:07 -0300

    [ REFAC ] Alterações

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
-	export const prerender = true;
 	import "../app.css";
 	import { Navbar } from "$lib";
+
+	export const prerender = true;
+	export const trailingSlash = "always";
 </script>
 
 <div class="max-w-screen-md min-h-full m-auto flex flex-col">
--- svelte.config.js
+++ svelte.config.js
@@ -11,10 +11,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: "index.html",
+			fallback: "404.html",
 		}),
 		paths: {
-			base: process.env.NODE_ENV === "production" ? "/ericogata.github.io" : "",
+			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
 		},
 	},
 };
